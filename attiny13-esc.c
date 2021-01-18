616fc5884349ea99cab80d7251880f6dfde054ae
elias <yemista@gmail.com>
2021-01-18 14:36:40 -0500

    Got the PWM output working
    -Can control PWM output using RC input signal
    -forward, reverse, and break are all  working

--- attiny13-esc.c
+++ attiny13-esc.c
@@ -24,6 +24,7 @@
 // how to interpret the RC input signal
 #define     RC_FOWARD_MIN          1600
 #define     RC_REVERSE_MIN         1400
+#define     RC_SIGNAL_OFF          0
 
 // min and max RC input signals
 #define     RC_MAX_VALUE           2000
@@ -31,21 +32,21 @@
 
 
 // max PWM value to check against in timer1 interrupr
-#define     MAX_PWM_VALUE          255  
+#define     MAX_PWM_VALUE          250  
 
 // how often to trigger compare match A interrupt
 #define     COMPARE_MATCH_A        0xff
 
-// function for translating input PWM to motor speed PWM
-#define     TRANSLATE_FORWARD_PWM(S)        (S - 1500) / 500 * MAX_PWM_VALUE
-#define     TRANSLATE_REVERSE_PWM(S)        (1500 - S) / 500 * MAX_PWM_VALUE
+// function for translating input PWM to motor sp(eed PWM
+#define     TRANSLATE_FORWARD_PWM(S)        ((S - 1500) / 2)
+#define     TRANSLATE_REVERSE_PWM(S)        ((1500 - S) / 2)
 
 
 // motor states
 #define     BREAK                   0x1
 #define     FORWARD                 0x2
 #define     REVERSE                 0x4
-
+#define     MOTORS_OFF              0
 
 
 
@@ -59,9 +60,14 @@ volatile uint16_t pulses = 0;
 
 // timer1 variables
 volatile uint8_t motors_on = FALSE;
-volatile uint8_t timer1_compa_matches = 0;
-volatile uint8_t pwm_threshold = 0;
+volatile uint16_t timer1_compa_matches = 0;
+volatile uint16_t pwm_threshold = 0;
 
+// watchdog timer variables
+volatile uint8_t pin_changed_int_fired = FALSE;
+
+// scalar for timer overflows -> ms
+uint16_t timer_scalar = 0;
 
 // inline methods for controlling h-bridge direction
 static inline void enableForwardGates() {
@@ -87,12 +93,14 @@ static inline void disableAllGates() {
 
 // called when an interrupt enabled pin changes its state
 ISR(PCINT0_vect){
+    pin_changed_int_fired = TRUE;
+
     if (PINB & PWM_INPUT) {
         timer1_compa_matches = 0;
         timer0_ovf = 0;   // reset timer overflow counter at rising edge pin change interrupt
         TCNT0 = 0;   // initialize counter register at rising edge pin change interrupt
     } else {
-        pulses = timer0_ovf * 27;
+        pulses = timer0_ovf * timer_scalar;
     }
 }
 
@@ -106,35 +114,49 @@ ISR(TIM0_COMPA_vect) {
     if (timer1_compa_matches > MAX_PWM_VALUE) {
         timer1_compa_matches = 0;
     }
-
+   
     if (timer1_compa_matches < pwm_threshold) {
         if (motors_on) {
             return;
         }
 
         motors_on = TRUE;
-        
+
         if (motor_direction & FORWARD) {
             enableForwardGates();
         } else if (motor_direction & REVERSE) {
             enableReverseGates();
-        } else {
+        } else if (motor_direction & BREAK) {
             enableBreakGates();
+        } else {
+            disableAllGates();
         }
     } else {
+        // if (motor_direction & BREAK) {
+        //     return;
+        // }
+
         if (!motors_on) {
             return;
         }
 
         motors_on = FALSE;
-
-        if (motor_direction & BREAK) {
-            return;
-        }
-
         disableAllGates();
     }
 }
+
+ISR(WDT_vect) {
+    if (pin_changed_int_fired) {
+        pin_changed_int_fired = FALSE;
+    } else {
+        pulses = 0;
+    }
+
+    // re-enable the watchdog interrupt
+    WDTCR |= (1 << WDTIE);
+}
+
+
 
 //initialization  methods
 static inline void enableInterrupts() {
@@ -160,43 +182,82 @@ static inline void setupTimer0() {
     GTCCR &= ~(1 << TSM);   // start timer
 }
 
+static inline void setupWatchdogTimerAsInterrupt() {
+    MCUSR = 0;          // clear MCU Status Register
+    WDTCR = 0;
+
+    // Setup Watchdog for interrupt and not reset, approximately 500ms timeout
+    WDTCR = (1 << WDTIE) | (1 << WDP2) | (1 << WDP0);
+}
+
+/**
+    Ftimer = CPU Frequency / Prescalar
+
+    Ftimer = 9.6MHz / 1 = 9.6MHz
+
+    tick = 1 / 9.6MHz = 0.000000104s
+
+    total = 0.000000104s * 255 = 0.00002652s
+
+    0.00002652s = 0.02652ms per tick
+
+    1 tick = .02652 ms
+    
+    1ms = 1 tick / 0.02652ms per tick = 37.7 ticks ~ 38 ticks
+
+    timer_scalar = 1000 / numTicks per ms = 1000 / 38 = 26
+*/
+static inline void setTimerScalar() {
+    timer_scalar = 26;
+}
+
 static inline void initialize() {
     cli();
     enableInterrupts();
     setupTimer0();
+    setupWatchdogTimerAsInterrupt();
     enableOutputs();
+    setTimerScalar();
     sei();
 }
 
-static void updateForward(uint8_t pwmVal, uint8_t currentState) {
-    if (currentState == REVERSE) {
+static void updateForward(uint16_t pwmVal) {
+    if (motor_direction == REVERSE) {
         motor_direction = BREAK;
         _delay_ms(10);
     }
 
+    pwmVal = pwmVal > RC_MAX_VALUE ? RC_MAX_VALUE : pwmVal;
     pwm_threshold = TRANSLATE_FORWARD_PWM(pwmVal);
     motor_direction = FORWARD;
 }
 
-static void updateReverse(uint8_t pwmVal, uint8_t currentState) {
-    if (currentState == FORWARD) {
+static void updateReverse(uint16_t pwmVal) {
+    if (motor_direction == FORWARD) {
         motor_direction = BREAK;
         _delay_ms(10);
     }
 
+    pwmVal = pwmVal < RC_MIN_VALUE ? RC_MIN_VALUE : pwmVal;
     pwm_threshold = TRANSLATE_REVERSE_PWM(pwmVal);
     motor_direction = REVERSE;
 }
 
-static uint8_t updateHBridge(uint8_t pwmVal, uint8_t currentState) {
-    if (pwmVal > RC_FOWARD_MIN) {
-        updateForward(pwmVal, currentState);
-        return FORWARD;
-    } else if (pwmVal < RC_REVERSE_MIN) {
-        updateReverse(pwmVal, currentState);
-        return REVERSE;
+static void disableHBridge() {
+    motors_on = FALSE;
+    motor_direction = MOTORS_OFF;
+    disableAllGates();
+}
+
+static void updateHBridge(uint16_t rcInput) {
+    if (rcInput == RC_SIGNAL_OFF) {
+        disableHBridge();
+    } else if (rcInput >= RC_FOWARD_MIN) {
+        updateForward(rcInput);
+    } else if (rcInput <= RC_REVERSE_MIN) {
+        updateReverse(rcInput);
     } else {
-        return BREAK;
+        motor_direction = BREAK;
     }
 }
 
@@ -206,7 +267,6 @@ int main(void) {
 
     uint16_t inputSignalPrev = 0;
     uint16_t inputSignal = inputSignalPrev;
-    uint8_t motor_direction = BREAK;
 
     // wait until we actually read a signal so we dont start incorrectly
     while (inputSignal == inputSignalPrev) {
@@ -217,8 +277,8 @@ int main(void) {
         inputSignal = pulses;
         
         if (inputSignal != inputSignalPrev) {
-            motor_direction = updateHBridge(inputSignal, motor_direction);
-            inputSignalPrev = inputSignal;        
+            updateHBridge(inputSignal);
+            inputSignalPrev = inputSignal;      
         }       
     }
 }
